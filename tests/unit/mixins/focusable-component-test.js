1fc357177535cd12c0cfb97240745dc8058f2ac0
Will Hastings <whastings@linkedin.com>
2015-10-30 07:22:37 -0700

    Write tests for focusable-component mixin.

--- tests/unit/mixins/focusable-component-test.js
+++ tests/unit/mixins/focusable-component-test.js
@@ -1,12 +1,47 @@
 import Ember from 'ember';
 import FocusableComponentMixin from 'ember-component-focus/mixins/focusable-component';
+import sinon from 'sinon';
 import { module, test } from 'qunit';
 
-module('Unit | Mixin | focusable component');
+var manager,
+    subject;
 
-// Replace this with your real tests.
-test('it works', function(assert) {
-  var FocusableComponentObject = Ember.Object.extend(FocusableComponentMixin);
-  var subject = FocusableComponentObject.create();
-  assert.ok(subject);
+module('Unit | Mixin | focusable component', {
+  beforeEach() {
+    subject = Ember.Object.extend(FocusableComponentMixin).create();
+
+    manager = {
+      focusComponent: sinon.spy(),
+      focusComponentAfterRender: sinon.spy()
+    };
+
+    subject.set('componentFocusManager', manager);
+  }
+});
+
+['focus', 'focusAfterRender'].forEach(function(method) {
+  var managerMethod = method.replace('focus', 'focusComponent');
+
+  test(`${method}() calls manager with itself and null by default`, function(assert) {
+    assert.expect(2);
+    subject[method]();
+
+    assert.ok(manager[managerMethod].calledOnce);
+    assert.ok(manager[managerMethod].calledWith(subject, null));
+  });
+
+  test(`${method}() calls manager with itself and value of focusNode`, function(assert) {
+    assert.expect(1);
+    subject.set('focusNode', 'foo');
+    subject[method]();
+
+    assert.ok(manager[managerMethod].calledWith(subject, 'foo'));
+  });
+
+  test(`${method}() calls manager with itself and passed child`, function(assert) {
+    assert.expect(1);
+    subject[method]('bar');
+
+    assert.ok(manager[managerMethod].calledWith(subject, 'bar'));
+  });
 });
--- tests/unit/services/component-focus/focus-manager-test.js
+++ tests/unit/services/component-focus/focus-manager-test.js
@@ -1,12 +1,14 @@
 import { moduleFor, test } from 'ember-qunit';
 
+var service;
+
 moduleFor('service:component-focus/focus-manager', 'Unit | Service | component focus/focus manager', {
-  // Specify the other units that are required for this test.
-  // needs: ['service:foo']
+  beforeEach() {
+    service = this.subject();
+  }
 });
 
 // Replace this with your real tests.
 test('it exists', function(assert) {
-  var service = this.subject();
   assert.ok(service);
 });
